f3e96315e8bcb9f37eb6c65dd8242b96475de53a
ě̸̛̙͕̝͈̖͖̙̎̔̎̓̎͆͝r̡̟͖͕̞̯̰̬̭͇̩͓̼̰̀̈́̇̋̓̊̑̌̋͠r̴̥͍̻͉̻̗͕̅͛̉̒̇̋̄̅͊̎̕͝͝ͅơ̵̡͐̒̊͐͛̎̽ŗ̛̘̠̖̮͚̤̙̬̱̞̥̙̹͎̄̐̈́̿͗͗͐̄͂̿̈́̕͠͝ <65093109+Drifty1803@users.noreply.github.com>
2022-05-15 18:32:33 +0300

    sprite update 2

--- Python Stuff/sprite resizer.py
+++ Python Stuff/sprite resizer.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 i = 0
 
@@ -9,7 +9,15 @@ for dr in os.listdir("C://Users//User//Desktop//Natsulogue//game//mod_assets//na
         
         img = Image.open("C://Users//User//Desktop//Natsulogue//game//mod_assets//natsuki//sitting//"+dr+"//"+k[i])
         width = 597
-        height = 605
+        rate = (width / float(img.size[0]))
+        height = int((float(img.size[1])*float(rate)))
+        #height = 605
         resized_img = img.resize((width, height), Image.ANTIALIAS)
-        resized_img.save("C://Users//User//Desktop//Natsulogue//game//mod_assets//natsuki//sitting//"+dr+"//"+k[i])
+        enhancer = ImageEnhance.Contrast(resized_img)
+        
+        level = 1.1
+        s_image = enhancer.enhance(level)
+        sharpness = ImageEnhance.Sharpness(s_image)
+        n_image = sharpness.enhance(level+0.1)
+        n_image.save("C://Users//User//Desktop//Natsulogue//game//mod_assets//natsuki//sitting//"+dr+"//"+k[i])
         i += 1
